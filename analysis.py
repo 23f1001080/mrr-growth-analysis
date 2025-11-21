f2bfeb89e1189669e1e527bdfc958b1838b791fd
Sakshi Agarwal <23f1001080@ds.study.iitm.ac.in>
2025-11-21 13:50:45 +0530

    Add files via upload

--- analysis.py
+++ analysis.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 
-# Quarterly MRR growth
+# Quarterlyyyyy MRR growthhhhh
 quarters = ["Q1", "Q2", "Q3", "Q4"]
 growth = [4.06, 8.76, 10.71, 8.02]
 industry_target = 15
